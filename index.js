ae37dba9784b58acd057e3e6d9ec84203473f273
RobertMeade <meade.develop3r@gmail.com>
2016-04-12 18:38:50 -0400

    initial regex to parse unaltered chords setup

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-
 var notes =  new Array("A", "Bflat", "B", "C", "Dflat", "D", "Eflat", "E", "F", "Gflat", "G");
 
 Note = function(pitch) {
     this.pitch = pitch;
-    this.getPitchString = function() {
-	return "" + notes[pitch % 12] + "_" + Math.floor( ( (pitch + 9) / 12 ) );
-    }
+    this.pitchString = "" + notes[pitch % 12] + "_" + Math.floor( ( (pitch + 9) / 12 ) );
 }
 Note.prototype = new Note();
 
+
+function generateChordStack(chord) {
+    var pattern = /([a-zA-Z]+)([0-9]+)/;
+    var result = chord.match(pattern);
+    console.log(result);
+}
+
 n = new Note(40);
 console.log(n);
-console.log(n.getPitchString());
+console.log(n.pitchString);
+
+generateChordStack("Fm7");
 
 var http = require("http");
 
 var server = http.createServer(function(req, res) {
     res.writeHead(200);
-    res.end(n.getPitchString());
+    res.end(n.pitchString);
 });
 
 server.listen(8080);
