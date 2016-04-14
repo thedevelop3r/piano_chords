f8ed9a62b918252eccc5bc0f5c7d376a9275e523
Robert <rjm27trekkie@gmail.com>
2016-04-14 01:57:27 -0400

    parsing for dim,min,maj,aug 6,7,11,13 completed

--- index.js
+++ index.js
@@ -5,9 +5,24 @@ Note = function(pitch) {
     this.pitch = pitch;
     this.note = pitch % 12;
     this.octave = Math.floor ( (pitch + 9) / 12 );
-    this.pitchString = "" + notes[this.note] + "_" + this.octave;
+    this.pitchString = notes[this.note] + "_" + this.octave;
+
+    this.alter = function (halfSteps) {
+	this.pitch += halfSteps;
+	this.note = this.pitch % 12;
+	this.octave = Math.floor ( (pitch + 9) / 12 );
+	this.pitchString = notes[this.note] + "_" + this.octave;
+    }    
 }
 Note.prototype = new Note();
+
+function printChord(stack) {
+    var output = new String();
+    for (i = 0; i < stack.length; i++) {
+	output += stack[i].pitchString + "\b\b, ";
+    }
+    console.log(output + "\b\b  ");
+}
 
 function findNoteValue(note) {
     for(i = 0; i < notes.length; i++) {
@@ -22,52 +37,91 @@ function findNoteValue(note) {
   return -1;
 }
 
-function buildMajorTriad(root, stack) {
-    stack.push(new Note(root.pitch));
-    console.log(root.pitch);
-    stack.push(new Note(root.pitch + 4));
-    stack.push(new Note(root.pitch + 7));
-}
+function buildBaseChord(root, type, chord) {
 
-// alter a pre-built major triad to minor, diminished, or augmented
-function alterTriad(stack, type) {
-    // lower 3rd and 5th for minor triad
+    // build triad
+    chord.push(new Note(root.pitch));
+    chord.push(new Note(root.pitch + 4));
+    chord.push(new Note(root.pitch + 7));
+
+    // handle 6 chords
+    if (type == 6) chord.push(new Note(root.pitch + 9));
+   
+    // handle 7,9,11,13 chords
+    else if (type == 7) { 
+	chord.push(new Note(root.pitch + 10));
+    }
+    else if (type == 9) { 
+	chord.push(new Note(root.pitch + 10));
+	chord.push(new Note(root.pitch + 14));
+    }
+    else if (type == 11) { 
+	chord.push(new Note(root.pitch + 10));
+	chord.push(new Note(root.pitch + 14));
+	chord.push(new Note(root.pitch + 17));
+    }
+    else if (type == 13) {
+	chord.push(new Note(root.pitch + 10));
+	chord.push(new Note(root.pitch + 14));
+	chord.push(new Note(root.pitch + 17));
+	chord.push(new Note(root.pitch + 21)); 	
+    }	
+}       
+
+// alter a pre-built chord chord
+function alterBaseChord(chord, type) {
+    // alter to diminished chord
     if (type  === "dim") {
-	stack[1] = new Note(stack[1].pitch - 1);
-	stack[2] = new Note(stack[2].pitch - 1);
+	for(i = 1; i <= 3; i++)
+	    chord[i].alter(-1);
     }
-    // lower 3rd for minor triad
+    // alter to minor chord
     else if (type === "m" || type == "min" || type == "-") {
-	stack[1] = new Note(stack[1].pitch - 1);
+	chord[1] = new Note(chord[1].pitch - 1);
     }
-    // raise 5th for augmented triad
+    // alter to augmented chord
     else if (type === "aug" || type === "+") {
-	stack[2] = new Note(stack[2].pitch + 1);
+	chord[2] = new Note(chord[2].pitch + 1);
+	chord[3] = new Note(chord[3].pitch + 1);
     }
-    // pass for major triad
+    // alter to major chord
+    else if (type == "maj" || type == "M") {
+	chord[3] = new Note(chord[3].pitch + 1);
+    }
 }
 
-function generateChordStack(chord) {
-    var pattern = /([A-Z#b]+)?([a-z\+\-]+)?([0-9]+)?/;
-    //pattern = /([A-Z#b])([a-z]+)([0-9]+)?/;
-    var result = chord.match(pattern);
-    console.log(result);
+function generateChord(chordString) {
+    // parse chord as list of aspects:
+    // [chord, root, alterations, build]
+    var pattern = /([ABCDEFG#b]+)?([a-zA-Z\+\-]+)?([0-9]+)?/;
+    var result = chordString.match(pattern);
+    //console.log(result);
 
-    var stack = new Array();
-    console.log(typeof result[1]);
-    buildMajorTriad(new Note(findNoteValue(result[1])), stack);
-    alterTriad(stack, result[2]);
-    console.log(stack);
+    // alter chord
+    var chord = new Array();
+    buildBaseChord(
+	new Note(findNoteValue(result[1])),
+	parseInt(result[3]),
+	chord);
+    alterBaseChord(chord, result[2]);
+    console.log(chordString + ": ");
+    printChord(chord);
 }
 
-//n = new Note(40);
-//console.log(n);
-//console.log(n.pitchString);
+generateChord("C7");
+generateChord("Cdim7");
+generateChord("CM7");
+generateChord("Cmaj7");
+generateChord("Cm7");
+generateChord("Cmin7");
+generateChord("Caug7");
+generateChord("C+7");
 
-generateChordStack("F#+");
+
+// webpage
+// ----------------------------
 
 var http = require("http");
-
 var server = http.createServer(function(req, res) {
     res.writeHead(200);
     res.end(n.pitchString);
